2c68941ae9fdcff48eb11a170702b5d75ed466c9
king8879 <k4507244@gmail.com>
2025-05-30 20:43:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+from os import getenv
+
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "22528446"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","0d81bf18019c5f3839037d0ae737c358")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN","8142310947:AAECuG6o74lGOk4GLxMWY5_r4PWc0xLQvwE")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+BOT_TEXT = " 🫵"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "633111330"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002517535092"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002517535092")) 
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002517535092"))
+# -----------------------------------------------
+join = ''
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://envs.sh/9HT.jpg")
+
+
+
+
+
+
